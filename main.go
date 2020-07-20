c7a6fd2d97a051df125480aad7e70b1dfd7ad22e
belove6893 <belove6893@gmail.com>
2020-07-20 13:49:04 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"time"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/streadway/amqp"
 )
 
@@ -146,7 +147,7 @@ func main() {
 		var peer Servicerespone
 		var info Registerinfo
 		var ccinfo Serviceinfo
-		db, err := sql.Open("mysql", "belove:oc886191@tcp(140.118.109.132:3306/crosschain")
+		db, err := sql.Open("mysql", "belove:oc886191@tcp(140.118.109.132:3306/crosschain)")
 		defer db.Close()
 		failOnError(err, "Failed to connect database")
 
